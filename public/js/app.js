31e6c7ef40818ab72ad9fe54e6af3470dccfab7d
yeasinArafat1997 <arafatyeasin1212@gma>
2021-07-13 16:14:24 +0600

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+const frm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+
+frm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const locationn = search.value;
+    messageOne.textContent='Loading...'
+    messageTwo.textContent=''
+    fetch('http://localhost:3000/weather?address='+locationn).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+        messageOne.textContent = data.error
+        }else{
+            messageOne.textContent = 'Location =>'+data.location
+            messageTwo.textContent = data.forecast
+        }
+    })
+})
+    
+})
---
+++ src/app.js
@@ -0,0 +1,121 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const gecode = require('./utiles/geocode');
+const weatherGecode = require('./utiles/forecast');
+const { error } = require('console');
+const app = express();
+
+//Define path for express config
+const publicDirectoryFile = path.join(__dirname,'../public');
+const viewPath = path.join(__dirname,'../templates/views');
+const partialsPath = path.join(__dirname,'../templates/partials');
+
+//Setup handlebar engine and views location
+app.set('view engine','hbs');
+app.set('views',viewPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryFile))
+
+app.get('/',(req,res)=>{
+    res.render('index',{
+        title:'Weather',
+        nam:'Arafat'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'About me',
+        nam:'Arafat'
+    })
+})
+
+
+app.get('/weather',(req,res)=>{
+
+    if(!req.query.address){
+        return res.send({
+            error:'You must give a address!!!'
+        })
+    }
+
+    gecode(req.query.address,(error,{location}={})=>{
+        if(error){
+            return res.send({error})
+        }
+
+        weatherGecode(req.query.address,(error,forecastData)=>{
+            if(error){
+                return res.send({error})
+            }
+            res.send({
+                forecast:forecastData,
+                location,
+                address:req.query.address
+            })
+        })
+    })
+
+
+
+    //res.send({
+       // forecast:'It is raining',
+       // location:'Bangladesh',
+        //address: req.query.address
+   // })
+})
+
+/*app.get('/products',(req,res)=>{
+
+    if(!req.query.search){
+        return res.send({
+            error:'You must provide a search time'
+        })
+    }
+    console.log(req.query.search);
+    res.send({
+        product:[]
+    });
+})*/
+
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        text:'This is some helpful text.',
+        title:'Help',
+        nam:'Arafat'
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+
+    res.render('404',{
+        title:'404',
+        nam:'Arafat',
+        errorMessage:'Help articale are not found'
+    })
+})
+
+
+
+
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+     
+        title:'404',
+        nam:'Arafat',
+        errorMessage:'Page not found!!!'
+    })
+})
+
+
+
+
+
+app.listen(3000,() =>{
+    console.log('Server is up on port 3000.');
+})
